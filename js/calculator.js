9e67e2da8039a503f73f89ff603fdad6df0a8316
Mike Smith <37947497+smithmich12@users.noreply.github.com>
2018-10-28 20:11:54 -0700

    Version 0.3.0
    - Added comments to code
    - Fixed caret issues on mobile
    - Fixed exponent raised to exponent. Example 2^3^2 now outputs correct answer
    - Fixed negative number raised to exponent by adding parentheses

--- js/calculator.js
+++ js/calculator.js
@@ -1,3 +1,11 @@
+/********************************
+ * Michael Smith
+ * Last Updated: October 28, 2018
+ * Lab 3 - CS 480
+ * The brains of the Calculator in Javascript
+ ********************************/
+
+// This is the screenDiv which is used in multiple functions and shortens code
 let screenDiv = document.getElementById("screen");
 
 // Event listeners to update caret position
@@ -6,19 +14,51 @@ screenDiv.addEventListener('keydown', update, false);
 screenDiv.addEventListener('mousedown', update, false);
 screenDiv.addEventListener('mouseup', update, false);
 
-
-// Click equals button if user presses enter in the input
-screenDiv.addEventListener("keyup", function(event) {
-  event.preventDefault();
-  if(event.keyCode === 13) {
-    document.getElementById("equals").click();
+// Called on every keyup/down or mouseup/down to update caret position
+function update(event) {
+  if(event.keyCode != 13){
+    document.getElementById("caretposition").innerHTML = getCaretPosition(screenDiv);
   }
-});
-
-function update() {
-  document.getElementById("caretposition").innerHTML = getCaretPosition(screenDiv);
 }
 
+
+// Event listener to check if an illegal character is pressed
+screenDiv.addEventListener("keypress", checkChar, false);
+
+// Called on every keypress, only allows numbers and operators
+// If enter is pressed it calls equals
+function checkChar(event) {
+  let keyCode = event.keyCode;
+  let otherKeys = [13, 40, 41, 42, 43, 45, 46, 47, 94]
+  console.log(keyCode + " " +  event.key);
+  if(keyCode != 0){
+    if (!(keyCode >= 48 && keyCode <= 57) && !(otherKeys.includes(keyCode))) {
+      event.preventDefault();
+    } else if (keyCode === 13){
+      event.preventDefault();
+      document.getElementById("equals").click();
+
+      // The following code sets the caret position to the end after enter is pressed
+      let range = document.createRange();
+      let sel = window.getSelection();
+      range.setStart(screenDiv.childNodes[0], screenDiv.innerHTML.length);
+      range.collapse(true);
+      sel.removeAllRanges();
+      sel.addRange(range);
+      screenDiv.focus();
+    }
+  }
+}
+
+// This is called when a double click on the back button occurs
+function clearScreen() {
+  let caretpositionDiv = document.getElementById("caretposition");
+  caretpositionDiv.innerHTML = 0;
+  screenDiv.innerHTML = "";
+}
+
+// This gets the caret position so a user can click in the middle of their
+// equation and their input will be where they expect it to be
 function getCaretPosition(screen){
   let caretPos = 0, sel, range;
   if(window.getSelection){
@@ -47,22 +87,29 @@ function getCaretPosition(screen){
 
 // This is called when a user clicks on one of the buttons and recieves what button was clicked
 function clickButton(operator) {
-  let caretpositionDiv = document.getElementById("caretposition")
+  let caretpositionDiv = document.getElementById("caretposition");
   switch(operator) {
-    case "AC":
-      screenDiv.innerHTML = "";
-      caretpositionDiv.innerHTML = "0"
+
+    // Back button deletes the last character
+    case "&lt;":
+      screenDiv.innerHTML = screenDiv.innerHTML.slice(0, -1);
+      if(caretpositionDiv.innerHTML != 0){
+        caretpositionDiv.innerHTML--;
+      }
       break;
+
+    // Equals attempts to solve the equation given
     case "=":
-      let expression = screenDiv.innerHTML;
-      if((expression != "") && !(expression == "Parenthesis Error" || expression == "Division by 0")){
-        screenDiv.innerHTML = solve(expression);
+      let expr = screenDiv.innerHTML;
+      if((expr != "") && !(expr == "Parenthesis Error" || expr == "Operator Syntax Error" || expr == "\u221E" || expr == "Division by 0")){
+        screenDiv.innerHTML = solve(expr);
       }
       caretpositionDiv.innerHTML = screenDiv.innerText.length;
       break;
+
+    // This is the default case to append the users input
     default:
       let exp = screenDiv.innerHTML;
-
       if(exp == "Parenthesis Error" || exp == "Operator Syntax Error" || exp == "\u221E" || exp == "Division by 0"){
         screenDiv.innerHTML = operator;
       } else if(exp.length < 21) {
@@ -78,7 +125,7 @@ function clickButton(operator) {
 
 
 
-
+// This takes the postfix expression and solves it.
 function solve(expression){
   let postfix = infixToPostfix(expression);
   if(postfix == -1) {
@@ -93,6 +140,7 @@ function solve(expression){
     } else if((!isNaN(character) && character != ' ') || character == 'N'){
       let num = "";
 
+      // This is to get each number with spaces decimals and converts the negative number back to negative
       while((!isNaN(character) && character != ' ') || character == '.' || character == 'N'){
         if(character == 'N'){
           num += '-';
@@ -108,7 +156,7 @@ function solve(expression){
     } else {
       let val1 = stack.pop();
       let val2 = stack.pop();
-      console.log(val1 + " " + val2);
+      //console.log(val1 + " " + val2);
       if(val1 == undefined || val2 == undefined){
         return "Operator Syntax Error";
       }
@@ -143,6 +191,8 @@ function solve(expression){
   return returnVal;
 }
 
+
+// This function converts the users infix expression to postfix
 function infixToPostfix(exp){
   expression = checkExpression(exp);
   let result = "";
@@ -198,15 +248,33 @@ function infixToPostfix(exp){
   return result;
 }
 
+// This function checks for certain cases, cases are described with comments
+// down below.
 function checkExpression(exp) {
   let len = exp.length;
+
+  if(exp.includes("^")){
+    console.log("yes");
+  }
+
+  // This is the correction for -2^4 vs (-2)^4
+  for(let i = 0; i < len; i++){
+    let k = i - 1;
+    if(exp.charAt(i) == "^" && exp.charAt(k) != ")" && k != 0){
+      while(!isNaN(exp.charAt(k))){
+        k--;
+      }
+      k++;
+      exp = exp.slice(0, k) + "(" + exp.slice(k, i) + ")" + exp.slice(i);
+      console.log(exp);
+    }
+  }
+
+
   for(let i = 0; i < len; i++){
     let temp = "";
-    // Check if multiplication should be added case like 8(2) should be 8*(2)
-
-
-    // Check for opening parens and add multiplication
     let newExp;
+    // Multiplication for parenthesis ex: 4(5)
     if(exp.charAt(i) == '(' && i != 0){
       if(!isNaN(exp.charAt(i - 1))){
         temp = exp.slice(0, i);
@@ -217,6 +285,7 @@ function checkExpression(exp) {
       }
     }
 
+    // Multiplication for parenthesis ex: (5)4
     if(exp.charAt(i) == ')' && i != exp.length - 1){
       if(!isNaN(exp.charAt(i + 1)) || exp.charAt(i + 1) == "(" || exp.charAt(i + 1) == "N"){
         temp = exp.slice(0, i + 1);
@@ -224,25 +293,19 @@ function checkExpression(exp) {
       }
     }
 
+    // Checks for negative number
     if(exp.charAt(i) == '-'){
       if(i == 0){
         exp = "N"  + exp.slice(1);
-        //console.log("NEGATIVE IN FIRST POS");
       }else if(isNaN(exp.charAt(i - 1))){
         exp = exp.slice(0, i) + "N" + exp.slice(i+1);
       }
     }
-
-
   }
   return exp;
 }
 
-
-function newExpr(exp, i){
-
-}
-
+// Order of operations
 function pemdas(character) {
   switch(character){
     case '+':
